5a22ab63c7907e9d3b56153404f97c2220329dd0
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2016-04-26 17:16:45 +0000

    add forwardMultiplyGate()

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Neural Networks
-A Neural Networks written in C++.
+# Hacker's guide to Neural Networks in C++
+ C++ version of [Hacker's guide to Neural Networks](http://karpathy.github.io/neuralnets/).
 
 ## Run
 It use [bazel](https://bazel.io) as a build tool
--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,20 @@
+/*
+ * C++ version of hacker's guide to Neural Networks
+ * http://karpathy.github.io/neuralnets/
+ */
 #include <iostream>
+
+/* Our goal:
+ * Somehow we tweak the input x and y to increase the output
+ * For x=-2, y=3 the output will be -6, we want to tweak x and y
+ * to increase the output to be greater than -6 */
+int forwardMultiplyGate(int x, int y)
+{
+    return x * y;
+}
 
 int main()
 {
-    std::cout << "Hello World\n";
+    std::cout << "Gate x*y for x=-2 y=3: ";
+    std::cout << forwardMultiplyGate(-2, 3) << "\n";
 }
